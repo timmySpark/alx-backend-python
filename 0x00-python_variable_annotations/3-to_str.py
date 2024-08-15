47e594043080bc73a4f497ac7b01f21bf89f6877
timmySpark <timmyspark1@gmail.com>
2024-08-15 23:50:15 +0000

    [feat] :- created a function that returns the string representation of the float value

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' Module containing a floor function (Typed Annotiation) '''
+
+
+def to_str(n: float) -> str:
+    ''' return a string repreentation of the float value '''
+    return str(n)

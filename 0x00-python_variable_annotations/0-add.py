b3927eb5804db96cd066060e9366b61fd86a7522
timmySpark <timmyspark1@gmail.com>
2024-08-15 23:23:05 +0000

    [feat] :- wrote a type annotiated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+    Module containing an add function (Typed Annotiation)
+'''
+
+def add( a: float, b: float ) -> float :
+    'Add two numbers (float)'
+    return (a + b)

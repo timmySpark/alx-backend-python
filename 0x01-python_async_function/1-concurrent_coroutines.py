ac3e084fd85496dd6c49fc851687eaed43a2c2a9
timmySpark <timmyspark1@gmail.com>
2024-08-20 00:19:54 +0000

    [feat] :- working on multiple caroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Working on Multiple coroutines'''
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List(Float):
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
